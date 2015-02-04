4a61e8fd97b8f6b8d43b86e464aa5303b35ce41e
Sean Baildon <sean@baildon.co>
2015-02-04 22:28:55 +0000

    Get file type

--- server.py
+++ server.py
@@ -10,27 +10,33 @@ from scapy.all import *
 path_to_mpds = 'mpds/'
 remote_host = 'http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys12/BigBuckBunny/bunny_2s/'
 
-def cb(pkt): 
-    get_found=str() 
-    host=str()
+def cb(pkt):
+	get_found=str()
+	host=str()
 
-    if pkt.haslayer(Raw):
-        load = pkt[Raw].load
-        try:
-            headers, body = load.split(r"\r\n\r\n", 1)
-        except: 
-            headers = load
-            body = '' 
-        header_lines = headers.split(r"\r\n") 
-        for h in header_lines:
-            if 'get /' in h.lower():
-                get_found = h.split(' ')[1]
-        if get_found:
-            for h in header_lines:
-                if 'host: ' in h.lower():
-                    host=h.split(":")[1].strip(" ").split("\r\n")[0]
-        if get_found:
-            print 'URL:'str(get_found)
+	if pkt.haslayer(Raw):
+		load = pkt[Raw].load
+		try:
+			headers, body = load.split(r"\r\n\r\n", 1)
+		except:
+			headers = load
+			body = ''
+		header_lines = headers.split(r"\r\n")
+		for h in header_lines:
+			if 'get /' in h.lower():
+				get_found = h.split(' ')[1]
+		if get_found:
+			for h in header_lines:
+				if 'host: ' in h.lower():
+					host=h.split(":")[1].strip(" ").split("\r\n")[0]
+			get_found = get_found.split('/')
+			get_file_type(get_found[-1])
+
+def get_file_type(file_):
+	if file_[-4:] == '.mpd':
+		request_for_mpd(file_)
+	elif file_[-4:] == '.m4s':
+		request_for_m4s(file_)
 
 def request_for_mpd(file_):
 	print 'request for mpd'
@@ -42,7 +48,7 @@ def request_for_m4s(file_):
 	print 'request for m4s'
 	obj = {'date': datetime.datetime.utcnow(), 'src-ip': request.remote_addr, 'request': file_ + '.m4s'}
 	manager.db_insert_get_request(obj)
-	
+
 def file_available_locally(path, file_):
 	return os.path.isfile(path + file_)
 
