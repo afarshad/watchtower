f3c721fe04010996bc4ba818f6543797da05d804
Sean Baildon <sean@baildon.co>
2015-02-05 17:05:28 +0000

    code cleanup
    prepend url schema in a much nicer way, some code cleanup

--- node/sniffer.py
+++ node/sniffer.py
@@ -11,8 +11,8 @@ from scapy.all import *
 path_to_mpds = 'mpds/'
 
 def packet_capture(packet):
-	get_found=str()
-	host=str()
+	get_found = str()
+	host = str()
 
 	if packet.haslayer(Raw):
 		load = packet[Raw].load
@@ -56,7 +56,9 @@ def get_file(url):
 	file_ = url.split('/')[-1]	
 
 	with open(path_to_mpds + file_, 'wb') as handle:
-		response = requests.get('http://' + url, verify=False, allow_redirects=True, stream=True)
+		if not "http" in url:
+			url = 'http://' + url
+		response = requests.get(url, verify=False, allow_redirects=True, stream=True)
 
 		if not response.ok:
 			return
@@ -83,7 +85,6 @@ class sniffing_thread(threading.Thread):
 		#sniff(iface="eth2.1000", filter='tcp port 80', prn=packet_capture, store=0)
 		sniff(filter='tcp port 80', prn=packet_capture, store=0)
 
-# handle_get_request('0.0.0.0', 'http://www-itec.uni-klu.ac.at', '/ftp/datasets/mmsys12/BigBuckBunny/bunny_2s/BigBuckBunny_2s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd', 'BigBuckBunny_2s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd')
 get_file('www-itec.uni-klu.ac.at/ftp/datasets/mmsys12/BigBuckBunny/bunny_2s/BigBuckBunny_2s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd')
 manager.parse_mpd(path_to_mpds + 'BigBuckBunny_2s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd')
 request_for_m4s('0.0.0.0', 'www.google.com', 'bunny_2s_50kbit/bunny_2s10.m4s', 'bunny_2s10.m4s')
