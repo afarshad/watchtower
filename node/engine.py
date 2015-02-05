863bc255db67e00c56c56073344acc8469a3dae8
Sean Baildon <sean@baildon.co>
2015-02-05 16:03:04 +0000

    refactor for consistent style

--- node/engine.py
+++ node/engine.py
@@ -1,22 +1,20 @@
-
 import numpy as np
+
 class MeasurementEngine(object):
     _stats=dict()
     videoQuality=float()
 
-
-
     def __init__(self):
-        _stats={'min_bitrate':None,
-                'max_bitrate':None,
-                'avg_bitrate':None,
-                'avg_videoQuality':None,
-                'avg_switchingImpact':None,
-                'no_changes':None,
-                'moving_avg_videoBitrate':list(),
-                'moving_avg_videoQuality':list(),
-                'moving_avg_switchingImpact':list(),
-                'videoBitrate':list()
+        _stats={'min_bitrate': None,
+                'max_bitrate': None,
+                'avg_bitrate': None,
+                'avg_videoQuality': None,
+                'avg_switchingImpact': None,
+                'no_changes': None,
+                'moving_avg_videoBitrate': list(),
+                'moving_avg_videoQuality': list(),
+                'moving_avg_switchingImpact': list(),
+                'videoBitrate': list()
         }
         videoQuality=0.0
 
@@ -24,8 +22,7 @@ class MeasurementEngine(object):
         """
             videoBitrate: Video Bitrate, shows video compression bitrate like 200kbps
             videoResolution: Video Resolution like 720p or 1080p
-            Return: videoQuality based on the work.
-        """
+            Return: videoQuality based on the work."""
 
         if videoResolution == 720 and videoBitrate < 2000 :
             self.videoQuality=-4.85*power(videoBitrate,-0.647)+1.011
@@ -34,9 +31,9 @@ class MeasurementEngine(object):
         if videoResolution == 360 and videoBitrate < 1000 :
             self.videoQuality=-17.53*power(videoBitrate,-1.048)+0.9912
         return
+
     def calculate_stats(self):
         self._stats['min_bitrate']=np.amin(slef._stats['videoBitrate'])
-
 
     def _getSwitchingImpact(self,start_time,curr_time,br_before,br_after):
 
@@ -47,7 +44,6 @@ class MeasurementEngine(object):
         and video bitrate after switching (br_after)."""
 
         return(np.absolute(br_after-br_before)*np.exp(-0.015*(curr_time-start_time)))
-
 
      def get_changes(self, list_):
         """Count the number of changes in a list object."""
