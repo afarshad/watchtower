e64aa43043e76f0e9577243877d93fe18500a59a
Sean Baildon <sean@baildon.co>
2015-02-23 12:47:30 +0000

    introduce default config overriding
    load sane config first, then update values specified by the user

---
+++ watchtower/config.py
@@ -0,0 +1,17 @@
+gui = {}
+api = {}
+database = {}
+
+def _setup_node_defaults():
+	global gui
+	gui['host'] = '0.0.0.0'
+	gui['port'] = 80
+
+	global api
+	api['host'] = '0.0.0.0'
+	api['port'] = 5000
+
+	global database
+	database['name'] = 'watchtower_node'
+	database['host'] = 'localhost'
+	database['port'] = 27017
