6c9bd0df8c79baee8e0fbdc83a3c369da797dcfa
afarshad <arsham.farshad@gmail.com>
2015-03-05 11:10:24 +0000

    changed engine

--- watchtower/node/engine.py
+++ watchtower/node/engine.py
@@ -12,24 +12,20 @@ def handle_this_method_call(_dictionary):
 
 
 def add_videoTime(_stats):
+
     """
     :return: A list of the
     the video time based on the received timestamps and segment duration time.
     It assumes that the all segments have the same size.
     """
-    print _stats
-    i = 0
-    passedTime = 0
+    passedTime=0
     for i in range(0, len(_stats), 1):
         if i is 0:
             passedTime = 0
         else:
             passedTime += _stats[i]['duration']
         _stats[i]['videoTime']=_stats[i]['timestamp']+ passedTime
-        i = i + 1
-
     return (_stats)
-
 
 def test_add_videoTime(_stats):
     _new_stats = add_videoTime(_stats)
@@ -37,59 +33,57 @@ def test_add_videoTime(_stats):
     print  _new_stats
 
 
+def calc_noStalls(_stats):
+    """
+    :input: _stats with the video time
 
-# def _calc_expanded_array(self):
-# 		for i in range(len(self._stats['timeStamp'])):
-# 			for i in range(self._stats['segmentSize']):
-# 				self._stats['videoTime'].append(self._stats['timeStamp'][i]+passedTime)
+    :return: number of times stall have happened
+    """
+    #If timeStamp[i+1] > videoTime[i] stall was happened, i is the segment index.
 
-
-# 	def _calc_noStalls(self):
-# 		"""
-
-# 		:return: number of times stall have happened
-# 		"""
-# 		#If timeStamp[i+1] > videoTime[i] stall was happened, i is the segment index.
-# 		#TODO add check for duplicate segment requests and so on.
-# 		for i in range(1,len(self._stats['timeStamp'])-1):
-# 			if self._stats['videoTime'][i] < self._stats['timeStamp'][i+1]:
-# 				self._stats['noStalls'].append(1)
-# 			else:
-# 				self._stats['noStalls'].append(0)
+    #TODO add check for duplicate segment requests and so on.
+    noStalls=0
+    for i in range(1,len(_stats)-1):
+        if _stats[i]['videoTime']< _stats[i+1]['timestamp']:
+            noStalls+=1
+    return noStalls
 
 
-# 	def _calcVideoQuality(self, video_resolution, video_bitrate):
-# 		"""
-# 		video_bitrate: Video Bitrate, shows video compression bitrate like 200kbps
-# 		video_resolution: Video Resolution like 720p or 1080p
-# 		Return: videoQuality.
-# 		"""
-# 		_videoQuality=0.0
-# 		if video_resolution == 720 and video_bitrate < 2000 :
-# 			_videoQuality=-4.85*math.pow(video_bitrate,-0.647)+1.011
-# 		if video_resolution == 1080 :
-# 			_videoQuality=-3.035*math.pow(video_bitrate,-0.5061)+1.022
-# 		if video_resolution == 360 and video_bitrate < 1000 :
-# 			_videoQuality=-17.53*math.pow(video_bitrate,-1.048)+0.9912
-# 		return _videoQuality
+def calc_videoQuality(_stats):
+    """
+    video_bitrate: Video Bitrate, shows video compression bitrate like 200kbps
+    video_resolution: Video Resolution like 720p or 1080p
+    Return: videoQuality.
+    """
+    _videoQuality=0.0
+    for i in range(0, len(_stats)):
+        if _stats[i]['height'] == 720 and _stats[i]['bitrate'] < 2000:
+            _videoQuality=-4.85*math.pow(int(_stats[i]['bitrate']),-0.647)+1.011
 
-# 	def calcStats(self):
-# 		#Calculate the stats for the video bitrate
-# 		self._stats['min_bitrate']=np.amin(self._stats['videoBitrate'])
-# 		self._stats['max_bitrate']=np.amax(self._stats['videoBitrate'])
-# 		self._stats['median_bitrate']=np.median(self._stats['videoBitrate'])
-# 		#Calculate the stats for the video resolution
-# 		self._stats['min_resolution']=np.amin(self._stats['videoResolution'])
-# 		self._stats['max_resolution']=np.amax(self._stats['videoResolution'])
-# 		self._stats['median_resolution']=np.median(self._stats['videoResolution'])
-# 		#Calculate no of changes in the video bitrates looking into the video bitrate list.
-# 		self._stats['no_changes']=self._getNoBitrateChanges(self._stats['videoBitrate'])
-# 		#Update the videoQuality list
-# 		self._stats['videoQuality']=list(
-# 			self._calcVideoQuality(np.array(self._stats['videoBitrate']),np.array(self._stats['videoResolution']))
-# 		)
+        if _stats[i]['height'] == 1080:
+            _videoQuality=-3.035*math.pow(int(_stats[i]['bitrate']),-0.5061)+1.022
 
-# 		#Update the switching impact
+        if _stats[i]['height'] == 360 and _stats[i]['bitrate'] < 1000:
+            _videoQuality=-17.53*math.pow(int(_stats[i]['bitrate']),-1.048)+0.9912
+
+    return _videoQuality
+
+def test_calc_videoQuality(_stats):
+
+    print calc_videoQuality(_stats)
+
+def calc_minVideoBitrate(_stats):
+    """
+
+    :param _stats: It is the list of parameters passed to calculate stats
+    :return: minimum video bitrate in the list
+    """
+    if len(_stats) > 0:
+        _minBitrate= min(_stats[:]['bitrate'])
+    else:
+        _minBitrate = 0
+    return _minBitrate
+
 
 
 # 	def _calcSwitchingImpact(self,start_time,curr_time,vq_before,vq_after):
